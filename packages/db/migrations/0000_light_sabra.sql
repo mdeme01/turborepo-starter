cb3cab879bb9bd2720f870affae2b18271784288
mdeme01 <deme.maraki01@gmail.com>
2024-06-23 10:58:27 +0200

    feat: add timestamps to db

--- packages/db/migrations/0000_light_sabra.sql
+++ packages/db/migrations/0000_light_sabra.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"created_at" date DEFAULT now(),
+	"updated_at" date DEFAULT now(),
 	"name" text,
 	"password" text
 );
--- packages/db/src/schema.ts
+++ packages/db/src/schema.ts
@@ -1,7 +1,9 @@
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { date, pgTable, text, uuid } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
     id: uuid('id').defaultRandom().primaryKey(),
+    created_at: date('created_at').defaultNow(),
+    updated_at: date('updated_at').defaultNow(),
     name: text('name'),
     password: text('password'),
 })
