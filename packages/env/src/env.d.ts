e1ba7767a21a70397053a5290150c0c64306e4bf
mdeme01 <deme.maraki01@gmail.com>
2024-07-21 14:32:18 +0200

    fix: env.d.ts

--- packages/env/src/env.d.ts
+++ packages/env/src/env.d.ts
@@ -1,11 +1,6 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         POSTGRES_URL: string
-        POSTGRES_HOST: string
-        POSTGRES_PORT: number
-        POSTGRES_DB: string
-        POSTGRES_USER: string
-        POSTGRES_PASSWORD: string
         SERVER_URL: string
         SERVER_HOST: string
         SERVER_PORT: number
