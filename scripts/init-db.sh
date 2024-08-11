9b57c68c77d0c1a0529301ecf13e932edc3033c9
mdeme01 <deme.maraki01@gmail.com>
2024-08-11 12:33:33 +0200

    fix: db, docker env

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -3,29 +3,10 @@
 set -e
 set -u
 
-function create_databases() {
-    database=$1
-    password=$2
-    echo "Creating user and database '$database' with password '$password'"
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-        CREATE USER $database with encrypted password '$password';
-        CREATE DATABASE $database;
-        GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+echo "Creating user and database '$POSTGRES_DATABASE' with password '$POSTGRES_PASSWORD'"
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER $POSTGRES_DATABASE with encrypted password '$POSTGRES_PASSWORD';
+    CREATE DATABASE $POSTGRES_DATABASE;
+    GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DATABASE TO $POSTGRES_DATABASE;
 EOSQL
-}
-
-if [ -n "$POSTGRES_DATABASES" ]; then
-    echo "Creating databases: $POSTGRES_DATABASES"
-    for db in $(echo $POSTGRES_DATABASES | tr ',' ' '); do
-        user=$(echo $db | awk -F":" '{print $1}')
-        pswd=$(echo $db | awk -F":" '{print $2}')
-        if [[ -z "$pswd" ]]
-            then
-                pswd=$user
-        fi
-
-        echo "User is $user and password is $pswd"
-        create_databases $user $pswd
-    done
-    echo "Databases created"
-fi
